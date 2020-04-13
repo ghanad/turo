d597e9a17954b990d08a39b32f34d8f20550499a
ghanad <48297368+ghanad@users.noreply.github.com>
2020-04-13 22:34:22 +0430

    nothing

--- diagonal_mask2.py
+++ diagonal_mask2.py
@@ -1,8 +1,10 @@
 from PIL import Image
 import PIL.ImageDraw as ImageDraw
+import ports
+
+step = 100
 
 im = Image.open("cisco3560x.png")
-
 
 im_a = Image.new("L", im.size, 0)
 draw = ImageDraw.Draw(im_a)
@@ -10,16 +12,22 @@ draw = ImageDraw.Draw(im_a)
 x_start = im.size[1] * -1
 x_end = 0
 
-step = 100
+
+# make diagonal
 renge_loop = round(im.size[0]/step)+im.size[1]
 
-
 for i in range(renge_loop):
-    point1 = ((x_start,0),(x_end,im.size[1]))
+    point1 = ((x_start,im.size[1]),(x_end,0))
     draw.line(point1, fill=200, width=10)
     x_start += step
     x_end += step
 
-im_a.show()
+
+
+im_rgba = im.copy()
+im_rgba.putalpha(im_a )
+
+
+im_rgba.show()
 
 ## https://note.nkmk.me/en/python-pillow-putalpha/
---
+++ diagonal_mask3.py
@@ -0,0 +1,37 @@
+from PIL import Image, ImageOps
+import PIL.ImageDraw as ImageDraw
+import ports
+
+step = 100
+
+im = Image.open("cisco3560x.png")
+
+draw = ImageDraw.Draw(im)
+
+x_start = im.size[1] * -1
+x_end = 0
+
+# make port
+draw.polygon(ports.port_geo(1), fill=(255,255,0))
+
+
+# make diagonal
+renge_loop = round(im.size[0]/step)+im.size[1]
+
+for i in range(renge_loop):
+    point1 = ((x_start,im.size[1]),(x_end,0))
+    draw.line(point1, fill=200, width=10)
+    x_start += step
+    x_end += step
+
+
+im_a = Image.new("L", im.size, 0)
+drawMask = ImageDraw.Draw(im_a)
+drawMask.polygon(ports.port_geo(1), fill=255)
+
+im2 = im.copy()
+im2.putalpha(im_a)
+
+im2.show()
+
+## https://note.nkmk.me/en/python-pillow-putalpha/
