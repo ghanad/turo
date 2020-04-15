2e8e523fca63e7f430bc6035ee8447ab4c7fc08c
unknown <am.ag65@gmail.com>
2020-04-15 10:53:29 +0430

    read from lnput

--- swithc_3560x.py
+++ swithc_3560x.py
@@ -1,17 +1,28 @@
 from PIL import Image, ImageOps
 import PIL.ImageDraw as ImageDraw
 import ports_3560x
+import vlan_colors
+import csv
+
 
 switch_image = Image.open("cisco3560x.png")
 
-#port 1
-port1 = ports_3560x.Port(24,'red',True)
-port_temp = port1.portShape()
-switch_image.paste(port_temp[0], port1.geo, port_temp[1])
 
-#port 13
-port13 = ports_3560x.Port(13,'blue',True)
-port_temp = port13.portShape()
-switch_image.paste(port_temp[0], port13.geo, port_temp[1])
+with open('sw1.csv', newline='') as ff:
+    f1 = ff.readlines()
+    switch_port_list = [x.rstrip() for x in f1]
+
+for i in switch_port_list:
+    portNum = int(i.split(',')[0])
+    color1 = str(i.split(',')[1].upper())
+    spare_status = str(i.split(',')[2])
+    if spare_status in ["spare", 1]:
+        spare_result = True
+    else:
+        spare_result = False
+    port1 = ports_3560x.Port(portNum, vlan_colors.vlanDict[color1] ,spare=spare_result)
+    port_temp = port1.portShape()
+    switch_image.paste(port_temp[0], port1.geo, port_temp[1])
+
 
 switch_image.show()
